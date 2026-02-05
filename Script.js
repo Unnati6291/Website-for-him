855ce3af28c0b008ea576a9ec73b22d144e8ffef
Unnati6291 <srivastavunnati1926@gmail.com>
2026-02-05 21:48:08 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,53 @@
+const startBtn = document.getElementById("startBtn");
+const card = document.getElementById("card");
+const container = document.querySelector(".container");
+const final = document.getElementById("final");
+const noBtn = document.getElementById("noBtn");
+const hearts = document.querySelector(".hearts");
+const loveMusic = document.getElementById("loveMusic");
+
+// 🔓 Browser unlock audio on first click
+startBtn.addEventListener("click", () => {
+  container.classList.add("hidden");
+  card.classList.remove("hidden");
+
+  loveMusic.muted = true;
+  loveMusic.play().then(() => {
+    loveMusic.pause();
+    loveMusic.currentTime = 0;
+    loveMusic.muted = false;
+  });
+});
+
+function yesClick() {
+  card.classList.add("hidden");
+  final.classList.remove("hidden");
+
+  loveMusic.currentTime = 0;
+  loveMusic.volume = 0.7;
+  loveMusic.play();
+}
+
+// 😈 NO button bhaagta rahe
+noBtn.addEventListener("mouseenter", () => {
+  const btnWidth = noBtn.offsetWidth;
+  const btnHeight = noBtn.offsetHeight;
+
+  const x = Math.random() * (window.innerWidth - btnWidth);
+  const y = Math.random() * (window.innerHeight - btnHeight);
+
+  noBtn.style.position = "fixed";
+  noBtn.style.left = x + "px";
+  noBtn.style.top = y + "px";
+});
+
+// ❤️ hearts
+setInterval(() => {
+  const heart = document.createElement("span");
+  heart.innerHTML = "❤️";
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.fontSize = Math.random() * 20 + 10 + "px";
+  hearts.appendChild(heart);
+
+  setTimeout(() => heart.remove(), 6000);
+}, 300);
